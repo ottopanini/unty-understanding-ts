854b8a3e1b0b310499d908e168dfa1f147bd2795
cpress <cpress@it4ipm.de>
2020-08-28 00:30:00 +0200

    Working with Numbers, Strings & Booleans

--- app.ts
+++ app.ts
@@ -1,14 +1,21 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     if (typeof n1 !== 'number' || typeof n2 !== 'number') {
         throw new Error('unexpected parameter type');
     }
 
-    return n1 + n2;
+    if (showResult) {
+        console.log(phrase + n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
 
-const result = add(number1, number2);
+const result = add(number1, number2, printResult,resultPhrase);
 
 console.log(result);
