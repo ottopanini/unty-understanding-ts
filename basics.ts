4985e60fcce0cf0375236d2e981c819bb860ceb6
cpress <cpress@it4ipm.de>
2020-08-28 10:28:09 +0200

    Working with Tuples - not specified

--- basics.ts
+++ basics.ts
@@ -1,10 +1,19 @@
 const person = {
     name: 'Maximilian',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 }
 
-let favoriteActivities: any[];
-favoriteActivities = ['Sports', 1];
+// both accepted
+person.role.push('admin');
+person.role[1] = 10;
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
 
 console.log(person.name); // error
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toLocaleUpperCase());
+}
