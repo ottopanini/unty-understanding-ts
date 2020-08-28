ac6d2889480f65d1e8c3e05532e6c135c8e65797
cpress <cpress@it4ipm.de>
2020-08-28 10:30:04 +0200

    Working with Tuples - with specified types

--- basics.ts
+++ basics.ts
@@ -1,4 +1,9 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'Maximilian',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
