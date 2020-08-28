6f5f8d5998e2eff10b02c08249eb5d6057701c4b
cpress <cpress@it4ipm.de>
2020-08-28 10:35:13 +0200

    Working with Enums- without

--- basics.ts
+++ basics.ts
@@ -1,18 +1,24 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// }
+const person = {
     name: 'Maximilian',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: 4
 }
 
 // both accepted
-person.role.push('admin');
-person.role[1] = 10;
+// person.role.push('admin');
+// person.role[1] = 10;
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
@@ -22,3 +28,4 @@ console.log(person.name); // error
 for (const hobby of person.hobbies) {
     console.log(hobby.toLocaleUpperCase());
 }
+
