81298e2f4bedb5ab4290a82b69ea86559cbf79da
cpress <cpress@it4ipm.de>
2020-08-30 22:58:15 +0200

    Compiling the Entire Project / Multiple Files

--- app.ts
+++ app.ts
@@ -1,20 +1,2 @@
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-userInput = 'Max';
-
-//works with unknown
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-    throw {
-        message,
-        errorCode: code
-    };
-}
-
-const err = generateError("An error occured", 500);
-console.log("m" + err);
+const userName = 'Maximilian';
+console.log(userName);
