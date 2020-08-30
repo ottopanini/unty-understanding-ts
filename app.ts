7569fc494cec1a0d11863982170919ed47683918
cpress <cpress@it4ipm.de>
2020-08-30 22:45:12 +0200

    The "never" Type

--- app.ts
+++ app.ts
@@ -8,3 +8,13 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+    throw {
+        message,
+        errorCode: code
+    };
+}
+
+const err = generateError("An error occured", 500);
+console.log("m" + err);
