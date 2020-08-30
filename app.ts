a50458d7e9e22ff6c1970e43a71926b6aa5dd296
cpress <cpress@it4ipm.de>
2020-08-30 22:33:41 +0200

    The "unknown" Type

--- app.ts
+++ app.ts
@@ -1,22 +1,10 @@
-function add(n1: number, n2: number): number {
-    return n1 + n1;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+
+//works with unknown
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-
-function printResult(num: number): void {
-    console.log('Result ' + num);
-}
-
-function returnNothing(num: number): undefined {
-    console.log('Result ' + num);
-
-    //needed here
-    return;
-}
-
-printResult(add(5, 12));
-
-let combineValues: (a: number, b: number) => number;
-combineValues = add;
-// combineValues = printResult;
-
-console.log(combineValues(8, 8));
