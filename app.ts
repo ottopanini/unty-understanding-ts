ff358e40956c7ea3ee30ef46fc99bccd89799cde
cpress <cpress@it4ipm.de>
2020-08-30 23:16:21 +0200

    Understanding TypeScript Core Libs - with defaults

--- app.ts
+++ app.ts
@@ -1,2 +1,7 @@
 const userName = 'Maximilian';
 console.log(userName);
+
+const button = document.querySelector<HTMLButtonElement>('button')!;
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
