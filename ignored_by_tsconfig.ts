e4ce3a21af9410ddacbbc53f105a2666db44e04d
cpress <cpress@it4ipm.de>
2020-08-31 12:34:43 +0200

    remove comments

--- ignored_by_tsconfig.ts
+++
@@ -1,1 +0,0 @@
-const otherUserName = 'Max';
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ const userName = 'Maximilian';
 console.log(userName);
 
 const button = document.querySelector<HTMLButtonElement>('button')!;
+
+// evt listener comment should be readable in src file only
 button.addEventListener('click', () => {
     console.log('Clicked!');
 });
