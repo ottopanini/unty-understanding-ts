fa45d0287cc5ac4b1079ae5fe459ab3fff6a60be
cpress <cpress@it4ipm.de>
2020-08-31 15:57:02 +0200

    Arrow functions - single param

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,10 @@ console.log(add(2, 5));
 const printOutput: (a: number|string) => void = output => console.log(output);
 
 printOutput(add(2, 5));
+
+const button = document.querySelector<HTMLButtonElement>('button');
+if (button) {
+    button.addEventListener('click', event => {
+        console.log(event);
+    });
+}
