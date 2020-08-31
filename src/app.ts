2886c0d223599ed2c4381a958285ee7719da048d
cpress <cpress@it4ipm.de>
2020-08-31 12:38:55 +0200

    noEmitOnError

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 const userName = 'Maximilian';
 console.log(userName);
 
-const button = document.querySelector<HTMLButtonElement>('button')!;
+const button = document.querySelector<HTMLButtonElement>('button');
 
 // evt listener comment should be readable in src file only
 button.addEventListener('click', () => {
