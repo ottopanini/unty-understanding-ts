9ec42e80b58cd7ff085d68dfcf712fae54568bb6
cpress <cpress@it4ipm.de>
2020-09-02 11:58:58 +0200

    Intersection Types - more flexible type combinations

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,24 @@
-interface Admin {
+type Admin = {
     name: string;
     priveleges: string[];
 };
 
-interface Employee {
+type Employee = {
     name: string;
     startDate: Date;
 }
 
-interface ElevatedEmployee extends Admin, Employee {
-
-}
+type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
     name: 'Max',
     priveleges: ['create-server'],
     startDate: new Date()
 }
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
 
 
